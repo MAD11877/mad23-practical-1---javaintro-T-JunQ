d966a8bd9a6abd8aacec7c46fba8a103e6dc3c18
TAN JUN QIN <116868179+T-JunQ@users.noreply.github.com>
2023-04-20 11:03:23 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,10 +4,10 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    System.out.println("Enter Height (M):");
+//     System.out.println("Enter Height (M):");
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
-    System.out.println("Enter Weight (KG):");
+//     System.out.println("Enter Weight (KG):");
     double weight = in.nextDouble();
 
     System.out.println(weight / (height*height));
